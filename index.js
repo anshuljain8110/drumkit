fede0113a04c962750936feeb352a1cd6564a49a
anshuljain8110 <116940602+anshuljain8110@users.noreply.github.com>
2023-05-14 17:27:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var audio = [new Audio('sounds/crash.mp3'),
-new Audio('sounds/kick-bass.mp3'),
-new Audio('sounds/snare.mp3'),
-new Audio('sounds/tom-1.mp3'),
-new Audio('sounds/tom-2.mp3'),
-new Audio('sounds/tom-3.mp3'),
-new Audio('sounds/tom-4.mp3')
+new Audio('kick-bass.mp3'),
+new Audio('snare.mp3'),
+new Audio('tom-1.mp3'),
+new Audio('tom-2.mp3'),
+new Audio('tom-3.mp3'),
+new Audio('tom-4.mp3')
 ]
 
 let x = document.getElementsByClassName("drum");
